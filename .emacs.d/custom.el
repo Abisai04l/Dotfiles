0ad4cc788d8937fd5d8dc6fc9c253dc3359311d4
Jeet-u <jeet.u1lnit@gmail.com>
2024-05-30 09:32:47 -0500

    Configuration

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(tracking helpful command-log-mode rainbow-delimiters ace-window dashboard telega projectile magit prettier-js tide rjsx-mode skewer-mode company emmet-mode web-mode counsel which-key visual-fill-column org-superstar all-the-icons-dired all-the-icons-ivy-rich doom-modeline doom-themes auto-package-update use-package)))
+   '(php-mode vterm-toggle tracking helpful command-log-mode rainbow-delimiters ace-window dashboard telega helm-projectile projectile magit lsp-ui yasnippet prettier tide rjsx-mode company emmet-mode web-mode counsel which-key visual-fill-column org-superstar all-the-icons-dired all-the-icons-ivy-rich all-the-icons doom-modeline doom-themes auto-package-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
